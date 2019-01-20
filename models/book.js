feb340bc9e69024dcd605d63ac8129b1828bd68f
Void <thevoidf@gmail.com>
2019-01-20 19:55:57 +0400

    missing comma

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	title: { type: String, required: true }
+	title: { type: String, required: true },
 	description: { type: String, required: true },
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
